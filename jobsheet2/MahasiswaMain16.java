fdab1e7c8b8ec480a69eb6f2e713a930b1e06e7e
hardenbakh <muhammadmuqribillah@gmail.com>
2026-02-18 21:16:16 +0700

    percobaan 3

--- jobsheet2/MahasiswaMain16.java
+++ jobsheet2/MahasiswaMain16.java
@@ -20,7 +20,7 @@ public class MahasiswaMain16 {
         mhs2.updateIpk(3.85);
         mhs2.tampilkanInformasi();
 
-        Mahasiswa16 mhs = new Mahasiswa16("Azkiyi",
-         "2141060031", 3.70, "TI 2C");
+        Mahasiswa16 mhsBakhtiarMuqri = new Mahasiswa16("Bakhtiar Muqri",
+         "2141060031", 3.70, "SIB 2C");
     }
 }
