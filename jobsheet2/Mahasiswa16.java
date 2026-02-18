7d3c86252e3e4e94bd2553e79b6d86503fe344dc
hardenbakh <muhammadmuqribillah@gmail.com>
2026-02-18 19:00:55 +0700

    modifikasi percobaan 1

--- jobsheet2/Mahasiswa16.java
+++ jobsheet2/Mahasiswa16.java
@@ -1,12 +1,23 @@
 package jobsheet2;
 
 public class Mahasiswa16 {
-      String nim;
+    String nim;
     String nama;
     String kelas;
     double ipk;
 
-    void tampilkaninformasi () {
+public Mahasiswa16(){
+
+}  
+
+public Mahasiswa16(String nm, String nim, double ipk, String kls) {
+    nama = nm ;
+    this. nim = nim;
+    this.ipk = ipk;
+    kelas = kls;
+}
+
+    void tampilkanInformasi () {
         System.out.println("Nama : " + nama);
         System.out.println("NIM : " + nim);
         System.out.println("IPK : " + ipk);
@@ -16,14 +27,20 @@ public class Mahasiswa16 {
             kelas = kelasBaru;
     }
     void updateIpk (double ipkBaru){
-        ipk = ipkBaru;
+       if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+         ipk = ipkBaru;
+       } else {
+        System.out.println("data tidak valid. IPK harus 0.0 sampai 4.0");
+       }
+
+       
     }
     String nilaiKinerja() {
         if (ipk >= 3.5) {
             return "Kinerja sangat baik";
         } else if (ipk >= 3.0) {
             return "kinerja baik";
-        } else if (ipk >= 20) {
+        } else if (ipk >= 2.0) {
             return "kinerja cukup";
         } else {
             return "kinerja kurang";
--- jobsheet2/MahasiswaMain16.java
+++ jobsheet2/MahasiswaMain16.java
@@ -9,9 +9,15 @@ public class MahasiswaMain16 {
         mhsl.kelas = "SIB 1-C";
         mhsl.ipk = 3.79;
 
-        mhsl.tampilkaninformasi();
+        mhsl.tampilkanInformasi();
         mhsl.ubahKelas("SIB 1E");
-        mhsl.updateIpk(3.85);
-        mhsl.tampilkaninformasi();
+        mhsl.updateIpk(4.85);
+        mhsl.tampilkanInformasi();
+
+        System.out.println();
+
+        Mahasiswa16 mhs2 = new Mahasiswa16("Nabila Azkiya Ramadhani", "2141720160", 3.80, "TI 1C");
+        mhs2.updateIpk(3.85);
+        mhs2.tampilkanInformasi();
     }
 }
